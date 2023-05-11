be5bedb3cd84ec48f3fb47e38f9ca90d5a11dcad
Paul Rowe <prowe@sourceallies.com>
2023-05-10 21:16:15 -0500

    adding an adjustment

--- Api/AccountEvents.cs
+++ Api/AccountEvents.cs
@@ -1,10 +1,20 @@
 
+using Microsoft.EntityFrameworkCore;
+
 public abstract class AccountEvent
 {
     public Guid Id { get; set; } = Guid.NewGuid();
     public Guid AccountId { get; set; }
-    public DateTimeOffset Timestamp { get; set; } = DateTimeOffset.Now;
+    public DateTime Timestamp { get; set; } = DateTime.UtcNow;
     public int PointChange { get; set; } = 0;
+
+    public async Task<int> RemainingBalance([Service] ApiDbContext dbContext)
+    {
+        var balance = await dbContext.AccountEvents
+            .Where(ae => ae.AccountId == this.AccountId && ae.Timestamp < this.Timestamp)
+            .SumAsync(ae => ae.PointChange);
+        return balance + PointChange;
+    }
 }
 
 public class DepositPointsEvent : AccountEvent
--- Api/Migrations/20230511021128_AdjustDateTime.Designer.cs
+++ Api/Migrations/20230511021128_AdjustDateTime.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -9,9 +10,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Api.Migrations
 {
     [DbContext(typeof(ApiDbContext))]
-    partial class ApiDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230511021128_AdjustDateTime")]
+    partial class AdjustDateTime
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "7.0.5");
